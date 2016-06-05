fe2788d850257010e02fb17d76f818856cd90e5b
Pieter Jansen van Vuuren <pieter.jvv@gmail.com>
2016-06-05 14:23:47 +1200

    Added DbConnectionFactory.SqlServer.Ninject

---
+++ src/DbConnectionFactory.SqlServer.Ninject/DbConnectionFactoryModule.cs
@@ -0,0 +1,10 @@
+﻿namespace DbConnectionFactory.SqlServer
+{
+    public class DbConnectionFactoryModule : Ninject.Modules.NinjectModule
+    {
+        public override void Load()
+        {
+            Kernel.Bind<IDatabaseConnectionFactory>().To<DatabaseConnectionFactory>();
+        }
+    }
+}
