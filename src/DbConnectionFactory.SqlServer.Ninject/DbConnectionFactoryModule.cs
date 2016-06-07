87f1f11b72c8977e0a28997d222cbba5a7488eba
Pieter Jansen van Vuuren <pieter.jvv@gmail.com>
2016-06-07 12:46:08 +1200

    Correctly setup the DbConnectionFactoryModule

--- src/DbConnectionFactory.SqlServer.Ninject/DbConnectionFactoryModule.cs
+++ src/DbConnectionFactory.SqlServer.Ninject/DbConnectionFactoryModule.cs
@@ -1,10 +1,77 @@
-﻿namespace DbConnectionFactory.SqlServer
+﻿using System.Linq;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace DbConnectionFactory.SqlServer
 {
     public class DbConnectionFactoryModule : Ninject.Modules.NinjectModule
     {
+        private readonly string _connectionString;
+        public const string DefaultConnectionStringName = "DefaultConnection";
+        private const string ConnectionStringName = "ConnectionStringName";
+
+        public DbConnectionFactoryModule() 
+            : this(GetConfigConnectionString())
+        {
+        }
+        /// <summary>
+        /// Use this only if you cannot use the default constructor
+        /// </summary>
+        /// <param name="connectionString"></param>
+        public DbConnectionFactoryModule(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        private static string GetConfigConnectionString()
+        {
+            var configurationManager = new ConfigurationManagerWrapper();
+            var connectionStringName = GetConnectionStringName(configurationManager);
+            return configurationManager.GetConnectionString(connectionStringName);
+        }
+
+        private static string GetConnectionStringName(IConfigurationManager configurationManager)
+        {
+            if (configurationManager.AppSettings.AllKeys.Contains(ConnectionStringName))
+            {
+                return configurationManager.AppSettings[ConnectionStringName];
+            }
+            return DefaultConnectionStringName;
+        }
+
         public override void Load()
         {
-            Kernel.Bind<IDatabaseConnectionFactory>().To<DatabaseConnectionFactory>();
+            Bind<IDatabaseConnectionFactory>()
+                .ToMethod(ctx => new DatabaseConnectionFactory(_connectionString));
         }
+        private sealed class ConfigurationManagerWrapper : IConfigurationManager
+        {
+            public NameValueCollection AppSettings
+            {
+                [DebuggerStepThrough]
+                get
+                {
+                    return ConfigurationManager.AppSettings;
+                }
+            }
+
+            [DebuggerStepThrough]
+            public string GetConnectionString(string name)
+            {
+                return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+            }
+
+            [DebuggerStepThrough]
+            public string GetProviderName(string name)
+            {
+                return ConfigurationManager.ConnectionStrings[name].ProviderName;
+            }
+
+            public T GetSection<T>(string sectionName) where T : class
+            {
+                return ConfigurationManager.GetSection(sectionName) as T;
+            }
+        }
+
     }
 }
---
+++ src/DbConnectionFactory.SqlServer.Ninject/IConfigurationManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Specialized;
+
+namespace DbConnectionFactory.SqlServer
+{
+    public interface IConfigurationManager
+    {
+        NameValueCollection AppSettings { get; }
+
+        string GetConnectionString(string name);
+
+        string GetProviderName(string name);
+
+        T GetSection<T>(string sectionName) where T : class;
+    }
+}
